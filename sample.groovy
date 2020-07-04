643fba5fb93dea535ce0f4210d081d777feee16c
mrsivag <65943744+mrsivag@users.noreply.github.com>
2020-07-04 19:55:07 +0530

    Create sample.groovy

---
+++ sample.groovy
@@ -0,0 +1,17 @@
+pipelineJob("Sample"){
+	description('Sample')
+	definition {
+	  cpsScm{
+	    scm{
+		 git{
+		  remote{
+		   url("https://github.com/mrsivag/jenkins-docker-groovy")
+		   credentials("GitUserPass")
+		  }
+		  branch("*/master")
+		 }
+		}
+		scriptPath("jenkinsfile")
+	  }
+	}
+}

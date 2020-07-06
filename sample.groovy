d244c48f92b3fa0172caa3b5000d3a87508906cb
mrsivag <65943744+mrsivag@users.noreply.github.com>
2020-07-06 19:27:30 +0530

    Update sample.groovy

--- sample.groovy
+++ sample.groovy
@@ -1,4 +1,9 @@
-pipelineJob("Sample"){
+folder('roboshop/CIJOBS') {
+    displayName('roboshop/CIJOBS')
+    description('roboshop CI Jobs Project Folder')
+}
+
+pipelineJob("robishop/CIJOB/Sample"){
 	description('Sample')
 	definition {
 	  cpsScm{
